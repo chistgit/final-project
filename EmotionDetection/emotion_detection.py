a40000d0b15e18a82ca9bd73f907d0772ac77759
Your Name <you@example.com>
2025-01-10 03:52:21 -0500

    Tasks 5-8

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,11 +8,20 @@ def emotion_detector(text):
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=headers)
     formatted_response = json.loads(response.text)
-    anger_score = float(formatted_response["emotionPredictions"][0]["emotion"]["anger"])
-    disgust_score = float(formatted_response["emotionPredictions"][0]["emotion"]["disgust"])
-    fear_score = float(formatted_response["emotionPredictions"][0]["emotion"]["fear"])
-    joy_score = float(formatted_response["emotionPredictions"][0]["emotion"]["joy"])
-    sadness_score = float(formatted_response["emotionPredictions"][0]["emotion"]["sadness"])
+
+    if response.status_code == 200:
+        anger_score = float(formatted_response["emotionPredictions"][0]["emotion"]["anger"])
+        disgust_score = float(formatted_response["emotionPredictions"][0]["emotion"]["disgust"])
+        fear_score = float(formatted_response["emotionPredictions"][0]["emotion"]["fear"])
+        joy_score = float(formatted_response["emotionPredictions"][0]["emotion"]["joy"])
+        sadness_score = float(formatted_response["emotionPredictions"][0]["emotion"]["sadness"])
+    elif response.status_code == 400:
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+    
     emotion_dict = {
         'anger': anger_score, 
         'disgust': disgust_score,
@@ -20,9 +29,14 @@ def emotion_detector(text):
         'joy': joy_score,
         'sadness': sadness_score
         }
-
-    dominant_emotion = max(emotion_dict, key=emotion_dict.get)
-    emotion_dict['dominant_emotion'] = dominant_emotion
+    
+    if all(value is None for value in emotion_dict.values()):
+        emotion_dict['dominant_emotion'] = None  # No dominant emotion
+    else:
+        dominant_emotion = max(
+        {k: v for k, v in emotion_dict.items() if v is not None},
+        key=lambda x: emotion_dict[x])
+        emotion_dict['dominant_emotion'] = dominant_emotion
 
   
     return  emotion_dict
